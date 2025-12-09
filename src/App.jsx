b814ba17632cc89147ed5ddce42d69bb92fe5bd5
Hanna Nyman <hanna.m.nyman@student.laurea.fi>
2025-12-09 21:43:21 +0200

    Fix base path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SnippetListView from "./SnippetListView";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <nav>
         <Link to="/">Snippets</Link> | <Link to="/add">Add Snippet</Link>
       </nav>
